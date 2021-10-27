78d3e7f9504de9e8d30ea44a6c791b5cc5dc963e
Seulvagem <ffelipe.dias@gmail.com>
2021-10-27 19:53:10 -0300

    some specs and funny aliases

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject io.github.seulvagem/clj-queue "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject io.github.seulvagem/clj-queue "0.1.1-SNAPSHOT"
+  :description "Some wrappers and utilities around clojure.lang.PersistentQueue"
+  :url "https://github.com/seulvagem/clj-queue"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
--- src/clj_queue/core.clj
+++ src/clj_queue/core.clj
@@ -1,16 +1,30 @@
-(ns clj-queue.core)
+(ns clj-queue.core
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]))
 
 (defn queue
+  "takes a coll or nothing, returns a queue"
   ([]
    (clojure.lang.PersistentQueue/EMPTY))
   ([coll]
    (into (queue) coll)))
 
+(def ueue
+  "alias for queue"
+  queue)
+
 (defn queue?
   [x]
   (= (type x) clojure.lang.PersistentQueue))
 
+(def ?
+  "alias for queue?"
+  queue?)
 
+(def queue-gen (gen/fmap #(queue %)
+                         (s/gen list?)))
+
+(s/def ::queue (s/with-gen queue? (constantly queue-gen)))
 
 ;; overwrite default printer for queues
 (defmethod print-method clojure.lang.PersistentQueue [q, w] ; Overload the printer for queues so they look like fish
