36cd055d74aca64c818c694db112c17daa77a56b
Seulvagem <ffelipe.dias@gmail.com>
2021-10-27 10:21:03 -0300

    the basics, creation and checking

--- src/clj_queue/core.clj
+++ src/clj_queue/core.clj
@@ -1,6 +1,19 @@
 (ns clj-queue.core)
 
-(defn foo
-  "I don't do a whole lot."
+(defn queue
+  ([]
+   (clojure.lang.PersistentQueue/EMPTY))
+  ([coll]
+   (into (queue) coll)))
+
+(defn queue?
   [x]
-  (println x "Hello, World!"))
+  (= (type x) clojure.lang.PersistentQueue))
+
+
+
+;; overwrite default printer for queues
+(defmethod print-method clojure.lang.PersistentQueue [q, w] ; Overload the printer for queues so they look like fish
+  (print-method '<- w)
+  (print-method (seq q) w)
+  (print-method '-< w))
